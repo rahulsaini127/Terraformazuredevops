414ba695f837c8b510707cdda887c3a36a7029d0
rahulsaini127 <rahulsaini127@gmail.com>
2024-06-23 13:59:41 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "demo-resources"
-    storage_account_name = "rahulstorage"
+    storage_account_name = "rahulsainistorage"
     container_name       = "prod-tfstate"
     key                  = "prod.terraform.tfstate"
   }
